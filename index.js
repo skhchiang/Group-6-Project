5d6591ff0ab2753230a28bd2b3c267e17c77fbfa
Stephen Chiang <skhchiang@gmail.com>
2018-05-06 21:31:31 +0800

    added profile, results, and index html files

--- index.js
+++ index.js
@@ -4,37 +4,16 @@ const app = express();
 const axios = require('axios');
 const async = require('async');
 
-// const getData = async.queue(function (data, callback) {
-//     console.log('Retrieving requested data' + data);
-//     axios.get('https://api.test.sabre.com/' + data)
-//         .then((response => {
-//             console.log(JSON.stringify(response.data), (err) => {
-//                 if (err) {
-//                     console.log(err);
-//                 }
 
-//                 callback();
-//             });
-//         }));
-// });
+//Materialize Dropdown 'Select' Initialize
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
 
+  // Or with jQuery
 
-// function callAPI() {
-//     axios.get('https://api.test.sabre.com/v1/shop/flights/cheapest/fares/LAX?pointofsalecountry=US ',
-//     { 
-//         'headers': {
-//             'Content-Type': ""application/x-www-form-urlencoded"",
-//             'Api-Key': ""5nfa6dfhq23m7deqf2ncvykc"",
-//             'X-signature': encrypted
-//         }   
-//     }).then((data)=> {
-//         console.log(data);
-//     }).catch((err) => {
-//         console.log(err);
-//     })
-// }
+//   $(document).ready(function(){
+//     $('select').formSelect();
+//   });
 
-
-// callAPI();"
-
-// http.listen(3000);
