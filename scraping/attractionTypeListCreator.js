2f42588bcf19a933fca07174051d7dacdaeffae7
Andrew Cheung <cheungdzinyung@gmail.com>
2018-05-07 16:27:22 +0800

    cleaned up cityList and activityTypeList

---
+++ scraping/attractionTypeListCreator.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+var content, attractions;
+var attractionTypes =[];
+var attractionTypesObjectArray = [];
+
+fs.readFile("./output.json", "utf-8", function read(err, data) {
+  if (err) {
+    throw err;
+  }
+
+  for (var i = 0; i < JSON.parse(data).length; i++) {
+    attractions = JSON.parse(data)[i].type;
+    attractionTypes.indexOf(attractions) === -1
+      ? attractionTypes.push(attractions)
+      : null;
+  }
+
+  for (var x = 0; x < attractionTypes.length; x++) {
+    attractionTypesObjectArray.push({typeOfActivities: attractionTypes[x]});
+  }
+
+  console.log(attractionTypesObjectArray);
+  fs.writeFile("attractionTypeList.json", JSON.stringify(attractionTypesObjectArray), function(err) {
+    console.log("File written");
+  });
+});
---
+++ scraping/cityListCreator.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+var content, cityName;
+var cityList = [];
+
+fs.readFile("./cities.json", "utf-8", function read(err, data) {
+  if (err) {
+    throw err;
+  }
+
+  for (var i = 0; i < JSON.parse(data).city.length; i++) {
+    cityName = JSON.parse(data).city[i];
+    cityName = cityName.replace("-", " ");
+    cityName = cityName.replace(/\b\w/g, l => l.toUpperCase());
+    cityList.push({city:cityName});
+  }
+  console.log(cityList);
+  fs.writeFile("cityList.json", JSON.stringify(cityList), function(err) {
+    console.log("File written");
+  });
+});
