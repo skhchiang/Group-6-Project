a5b607a9f07b6c715b60709896070498f799f004
lukmingchuelise <ymsmemories@gmail.com>
2018-05-12 22:25:18 +0800

    m

--- services/builderService.js
+++ services/builderService.js
@@ -43,35 +43,40 @@ class BuilderService {
     }
   }
 
-  create(data, user) {
+  create(data,user) {
+    console.log("data.activities", data['activities[]']);
     return this.knex.transaction(function(trx) {
-      return trx
-        .insert([{ name: data.name }], "id") //"id" = return  the new iti's id
-        .into("itineraries")
-        .then(function(ids) {
-          console.log("ids", ids);
-          return Promise.map(data.activities, function(activity) {
-            let itinAct = {
-              itineraries_id: ids[0], // assume inserted 2/3 itin, ids[0] is the id of first itin
-              activities_id: activity
-            };
-            return trx
-              .insert(itinAct)
-              .into("itineraries_activities")
-              .returning("itineraries_id");
-          });
-        })
-        .then(function(ids) {
-          console.log(ids);
+        return trx 
+          .insert([{ name: data.itinName, description: data.itinDesc }], "id") //"id" = return  the new iti's id
+          .into("itineraries")
+          .then(function(ids) {
+              console.log(
+                  "ids", ids
+              )
+            return Promise.map(data['activities[]'], function(activity) {
+              let itinAct = {
+                itineraries_id: ids[0],       // assume inserted 2/3 itin, ids[0] is the id of first itin
+                activities_id: activity
+              };
+              return trx.insert(itinAct).into("itineraries_activities").returning("itineraries_id");
+            });
+          })
+          .then(function(ids){
 
-          let itiUser = {
-            itineraries_id: ids[0][0],
-            users_id: user,
-            is_create: true
-          };
-          return trx.insert(itiUser).into("users_itineraries");
+            console.log(ids);
+              
+                  let itiUser={
+                      itineraries_id:ids[0][0],
+                      users_id:user,
+                      is_create:true
+                      
+                  };
+              return trx.insert(itiUser).into("users_itineraries");
+
         });
-    });
+          
+      })
+
   }
 }
 
